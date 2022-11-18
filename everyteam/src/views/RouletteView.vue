1e50445a178a3df82f51d79cef261efce592b115
practice1 <shson1217@kw.ac.kr>
2022-11-18 14:50:57 +0900

    feat : seongho_test

---
+++ everyteam/src/views/RouletteView.vue
@@ -0,0 +1,170 @@
+<template>
+  <v-app>
+    <v-main>
+      <div id="app">
+        <h1>룰렛</h1>
+        <div class="roulette-outer">
+            <div class="roulette-pin"></div>
+            <div class="roulette" v-bind:style="rouletteStyle">
+
+                <!--값 영역-->
+                <div class="item-wrapper">
+                    <div class="item" v-for="(item,index) in items" v-bind:style="itemStyles[index]">{{item.value}}
+                    </div>
+                </div>
+
+                <!--선 영역-->
+                <div class="line-wrapper">
+                    <div class="line" v-for="(item,index) in items" v-bind:style="lineStyles[index]"></div>
+                </div>
+            </div>
+        </div>
+        <button class="play-btn" v-on:click="play" v-bind:disabled="buttonDisable">돌려돌려 돌림판~</button>
+        <hr>
+        <ul>
+            <li v-for="(h,idx) in history">{{h}}</li>
+        </ul>
+    </div>
+    </v-main>
+  </v-app>
+</template>
+
+    <script>
+        Vue.createApp({
+            data() {
+                return {
+                    items: [
+                        { value: "100점" },
+                        { value: "200점" },
+                        { value: "-500점" },
+                        { value: "0점" },
+                    ],
+                    itemStyles: [],
+                    lineStyles: [],
+                    current: 0, //실제 가리키는 데이터
+                    count: 0,
+                    history: [],
+                    buttonDisable: false,
+                };
+            },
+            computed: {
+                segment() {
+                    return 360 / this.items.length;
+                },
+                offset() {
+                    return this.segment / 2;
+                },
+                angle() {
+                    // return -this.current * this.segment; 각도가 너무일정함.
+                    let temp = -this.current * this.segment;
+                    let randomOffset = Math.floor(Math.random() * this.segment) - this.offset - 1;
+                    let cycle = this.count * 360 * 5; //5바퀴
+                    return -(temp + randomOffset + cycle) //랜덤변화
+                },
+                rouletteStyle() {
+                    return {
+                        "transform": "rotate(" + this.angle + "deg)"
+                    };
+                },
+                currentItem() {
+                    return this.items[this.current];
+                },
+            },
+            methods: {
+                play() {
+                    this.buttonDisable = true;
+                    this.count++;
+                    this.current = Math.floor(Math.random() * this.items.length);
+                    this.history.push(this.currentItem.value);
+                    setTimeout(() => {
+                        this.buttonDisable = false;
+                    }, 5000);
+                },
+            },
+            created() {
+                //itemStyles정의
+                this.items.forEach((el, idx) => {
+                    this.itemStyles.push({
+                        "transform": "rotate(" + this.segment * idx + "deg)",
+                    });
+                    //lineStyles정의
+                    this.lineStyles.push({
+                        "transform": "rotate(" + (this.segment * idx + this.offset) + "deg)",
+                    });
+                });
+            },
+        }).mount("#app");
+    </script>
+
+    <style>
+        html,
+        body {
+            margin: 0;
+            padding: 0;
+        }
+
+        #app {
+            text-align: center;
+        }
+
+        .roulette-outer {
+            position: relative;
+            overflow: hidden;
+            width: 500px;
+            height: 500px;
+            font-size: 30px;
+            margin-left: auto;
+            margin-right: auto;
+            background: #ffeaa7;
+        }
+
+        .roulette-outer>.roulette {
+            position: absolute;
+            top: 5%;
+            left: 5%;
+            right: 5%;
+            bottom: 5%;
+            border-radius: 50%;
+            border: 2px solid black;
+        }
+
+        .roulette-outer>.roulette-pin {
+            position: absolute;
+            top: 0;
+            left: 50%;
+            width: 0;
+            height: 0;
+            margin-left: -4px;
+            border-style: solid;
+            border-width: 25px 5px 0 5px;
+            border-color: #007bff transparent transparent transparent;
+        }
+
+        .roulette-outer>.roulette>.item-wrapper>.item {
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+
+            padding-top: 10%;
+            text-align: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .roulette-outer>.roulette>.line-wrapper>.line {
+            position: absolute;
+            top: 0;
+            bottom: 50%;
+            left: 50%;
+            width: 2px;
+            margin-left: -1px;
+            background: black;
+            transform-origin: bottom;
+        }
+
+        .roulette-outer>.roulette {
+            transition: transform 5s ease-in-out;
+        }
+    </style>
